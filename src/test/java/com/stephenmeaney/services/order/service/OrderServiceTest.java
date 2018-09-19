1bb774a85e15d28fb3d84dd56cf07cc135cc34bb
Stephen Meaney <smeaney@solstice.com>
2018-09-19 09:00:53 -0500

    Attempting to configure dbUnit properties

---
+++ src/test/java/com/stephenmeaney/services/order/service/OrderServiceTest.java
@@ -0,0 +1,12 @@
+package com.stephenmeaney.services.order.service;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
